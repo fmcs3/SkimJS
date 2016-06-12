5579f51b55e37e038f743a0b879a0baada6cf69a
luisdelgado <lhds@cin.ufpe.br>
2016-06-12 09:10:24 -0300

    Programa 4 - Testando while e for

--- Main.js
+++ Main.js
@@ -36,3 +36,21 @@ for (; x < 15;) {
 	x = x+1
 }
 */
+
+//Programa 4 - Testando while e for
+/*
+var x = 0, y = 10;
+x + y;
+if (x == 1) {
+	var z = true;
+}
+else {
+	var x = 10;
+}
+while (x < 15) {
+	x = x+1;
+	for (; x < 20;) {
+		x = x+1
+	}
+}
+*/
