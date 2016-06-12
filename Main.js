a66460b7e69ffce73b97a1f98b84906606be6a4e
luisdelgado <lhds@cin.ufpe.br>
2016-06-11 21:11:19 -0300

    Programa 3 - Testando for

--- Main.js
+++ Main.js
@@ -21,3 +21,18 @@ while (x < 15) {
 	x = x+1;
 }
 */
+
+//Programa 3 - Testando for
+/*
+var x = 0, y = 10;
+x + y;
+if (x == 1) {
+	var z = true;
+}
+else {
+	var x = 10;
+}
+for (; x < 15;) {
+	x = x+1
+}
+*/
